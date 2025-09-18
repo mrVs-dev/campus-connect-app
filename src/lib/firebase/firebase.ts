fff12b00f99132dab308e17ab86e504b2af895f5
Vannak Soth <vannak@api-school.com>
2025-09-18 09:47:46 +0000

    Try fixing this error: `Runtime Error: FirebaseError: Firebase: Error (a

--- src/lib/firebase/firebase.ts
+++ src/lib/firebase/firebase.ts
@@ -14,7 +14,16 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase for client-side
-const app: FirebaseApp = !getApps().length ? initializeApp(firebaseConfig) : getApp();
-const auth: Auth = getAuth(app);
+let app: FirebaseApp;
+let auth: Auth;
 
+if (typeof window !== 'undefined' && !getApps().length) {
+  app = initializeApp(firebaseConfig);
+  auth = getAuth(app);
+} else if (typeof window !== 'undefined') {
+  app = getApp();
+  auth = getAuth(app);
+}
+
+// @ts-ignore
 export { app, auth };
