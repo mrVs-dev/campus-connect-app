d0ca6c0bba9abba4aafd173d087aea1781a0e23a
Vannak Soth <vannak@api-school.com>
2025-09-18 09:48:26 +0000

    Try fixing this error: `Runtime Error: FirebaseError: Firebase: Error (a

--- src/lib/firebase/firebase.ts
+++ src/lib/firebase/firebase.ts
@@ -4,20 +4,20 @@
 import { initializeApp, getApps, getApp, type FirebaseApp } from "firebase/app";
 import { getAuth, type Auth } from "firebase/auth";
 
-const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
-};
-
-// Initialize Firebase for client-side
 let app: FirebaseApp;
 let auth: Auth;
 
 if (typeof window !== 'undefined' && !getApps().length) {
+  // This config is only created and used on the client side.
+  const firebaseConfig = {
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+  };
+  
   app = initializeApp(firebaseConfig);
   auth = getAuth(app);
 } else if (typeof window !== 'undefined') {
