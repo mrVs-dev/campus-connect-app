590cfdcbfb25ea3d6f81b1e9685d1b100146d242
Vannak Soth <vannak@api-school.com>
2025-09-18 09:50:31 +0000

    is there anything I can do to help?

--- src/lib/firebase/firebase.ts
+++ src/lib/firebase/firebase.ts
@@ -4,20 +4,21 @@
 import { initializeApp, getApps, getApp, type FirebaseApp } from "firebase/app";
 import { getAuth, type Auth } from "firebase/auth";
 
+// This config is only used on the client side.
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+};
+
 let app: FirebaseApp;
 let auth: Auth;
 
+// Initialize Firebase on the client side only.
 if (typeof window !== 'undefined' && !getApps().length) {
-  // This config is only created and used on the client side.
-  const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
-  };
-  
   app = initializeApp(firebaseConfig);
   auth = getAuth(app);
 } else if (typeof window !== 'undefined') {
