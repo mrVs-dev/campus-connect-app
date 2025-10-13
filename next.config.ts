aeb071212541979135e60253d6b81ec363069fad
Vannak Soth <vannak@api-school.com>
2025-10-13 07:48:48 +0000

    still looping

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
@@ -28,11 +29,6 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
-    ],
-  },
-  devIndicators: {
-    allowedDevOrigins: [
-      'https://6000-firebase-studio-1758175001543.cluster-nle52mxuvfhlkrzyrq6g2cwb52.cloudworkstations.dev',
     ],
   },
 };
