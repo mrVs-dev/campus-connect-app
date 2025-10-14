bd9ca06c0bd00a99da1928809ca38b1ecf34f402
Vannak Soth <vannak@api-school.com>
2025-10-14 09:04:56 +0000

    they are no longer blank, but there were alot more communes and villages

--- src/lib/address-data.ts
+++ src/lib/address-data.ts
@@ -1,10 +1,72 @@
 
 export const addressData = {
   communes: [
-    { id: "1", name: 'Sla Kram', villages: ['Treang', 'Sla Kram', 'Bangkaong', 'Chong Kaosou', 'Dol Po', 'Tramneak'] },
-    { id: "2", name: 'Svay Dangkum', villages: ['Svay Dangkum', 'Mondul 1', 'Mondul 2', 'Thnol', 'Taphul', 'Vihear Chen'] },
-    { id: "3", name: 'Sala Kamreuk', villages: ['Sala Kamreuk', 'Wat Bo', 'Wat Damnak', 'Chongkaosou', 'Taphul'] },
-    { id: "4", name: 'Chreav', villages: ['Chreav', 'Pou Banteaychey', 'Prey Kuy'] },
-    { id: "5", name: 'Nokor Thum', villages: ['Nokor Krau', 'Nokor Knong', 'Veal'] },
+    {
+      id: '1',
+      name: 'Sla Kram',
+      villages: [
+        'Treang',
+        'Sla Kram',
+        'Bangkaong',
+        'Chong Kaosou',
+        'Dol Po',
+        'Tramneak',
+      ],
+    },
+    {
+      id: '2',
+      name: 'Svay Dangkum',
+      villages: [
+        'Svay Dangkum',
+        'Mondul 1',
+        'Mondul 2',
+        'Thnol',
+        'Taphul',
+        'Vihear Chen',
+        'Kok Chak',
+        'Pa O',
+        'Kruos',
+        'Puok',
+        'Ta Vien',
+      ],
+    },
+    {
+      id: '3',
+      name: 'Sala Kamreuk',
+      villages: [
+        'Sala Kamreuk',
+        'Wat Bo',
+        'Wat Damnak',
+        'Chongkaosou',
+        'Taphul',
+        'Chreav',
+        'Knar',
+        'Prey Kuy',
+      ],
+    },
+    {
+      id: '4',
+      name: 'Chreav',
+      villages: ['Chreav', 'Pou Banteaychey', 'Prey Kuy', 'Kandek', 'Sambour'],
+    },
+    {
+      id: '5',
+      name: 'Nokor Thum',
+      villages: ['Nokor Krau', 'Nokor Knong', 'Veal', 'Rohal'],
+    },
+    {
+      id: '6',
+      name: 'Kouk Chak',
+      villages: ['Trapeang Ses', 'Pa O', 'Kouk Chak', 'Thmei'],
+    },
+    { id: '7', name: 'Ampil', villages: ['Ampil', 'Thnal', 'Veal'] },
+    { id: '8', name: 'Tuek Vil', villages: ['Tuek Vil', 'Sandan', 'Pa O'] },
+    { id: '9', name: 'Sambour', villages: ['Sambour', 'Prey Thom', 'Khnar'] },
+    {
+      id: '10',
+      name: 'Krabei Riel',
+      villages: ['Krabei Riel', 'Roka', 'Pram', 'Prey Pou'],
+    },
+    { id: '11', name: 'Chong Khnies', villages: ['Chong Khnies', 'Kbal Spean'] },
   ],
 };
