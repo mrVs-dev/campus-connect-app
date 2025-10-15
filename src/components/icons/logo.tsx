0247d1bffb078597cfc713747324374668762f75
Vannak Soth <vannak@api-school.com>
2025-10-15 02:11:49 +0000

    for the main page/dashboard where do i put it? is it under Admin? as in

--- src/components/icons/logo.tsx
+++ src/components/icons/logo.tsx
@@ -51,7 +51,7 @@ function GuardianLogo(props: SVGProps<SVGSVGElement>) {
 
 function DefaultLogo(props: SVGProps<SVGSVGElement>) {
   return (
-    // Replace this with your Default logo SVG code
+    // This is the fallback logo. For your use case, it should be the same as the AdminLogo.
     <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" {...props}>
       <path d="M4 19.5v-15A2.5 2.5 0 0 1 6.5 2H20v20H6.5a2.5 2.5 0 0 1 0-5H20" />
     </svg>
