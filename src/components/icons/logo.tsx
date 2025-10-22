57cbc8dc67ad1d2e551543c361c2dd62d0711729
Vannak Soth <vannak@api-school.com>
2025-10-22 07:12:07 +0000

    would it be better to upload a png instead of svg

--- src/components/icons/logo.tsx
+++ src/components/icons/logo.tsx
@@ -1,10 +1,16 @@
 import * as React from 'react';
+import Image from 'next/image';
 import { cn } from '@/lib/utils';
 
 export function Logo({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
   return (
     <div className={cn('relative h-12 w-12', className)} {...props}>
-      {/* This is a placeholder. The real logo component was causing issues. */}
+      <Image
+        src="/API_Round_Logo.png"
+        alt="API School Logo"
+        width={48}
+        height={48}
+      />
     </div>
   );
 }
